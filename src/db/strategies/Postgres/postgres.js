f4c141b6becf03088871309cec951f39e8d3d208
hernani <megahack9@outlook.com>
2020-06-28 10:42:07 -0300

    server concluido-continuacao repositorio modulo13gamaacademy

---
+++ src/db/strategies/Postgres/postgres.js
@@ -0,0 +1,65 @@
+const ICrud = require('./../Interfaces/InterfaceCrud')
+const Sequelize = require('sequelize')
+
+class Postgres extends ICrud {
+  constructor(connection, schema) {
+    super()
+    this._connection = connection
+    this._schema = schema
+    //this._connect()
+  }
+  async isConnected() {
+    try {
+      await this._connection.authenticate()
+      return true
+    }
+    catch (error) {
+      console.log('fail', error)
+      return false
+    }
+  }
+  static async defineModel(connection, schema) {
+    const model = connection.define(
+      schema.name, schema.schema, schema.options
+    )
+    await model.sync()
+    return model
+  }
+  async create(item) {
+    const {
+      dataValues
+    } = await this._schema.create(item)
+    return dataValues
+  }
+
+  async update(id, item, upsert = false) {
+    const fn = upsert ? 'upsert' : 'update'
+    return this._schema[fn](item, { where: { id: id } })
+  }
+
+  async delete(id) {
+    const query = id ? { id } : {}
+    return this._schema.destroy({ where: query })
+  }
+
+  async read(item = {}) {
+    return this._schema.findAll({ where: item, raw: true })
+  }
+  static connect() {
+    const connection = new Sequelize(process.env.POSTGRES_URL, {
+      quoteIdentifiers: false,
+      ssl: process.env.SSL_DB,
+      dialectOptions: {
+        ssl: process.env.SSL_DB,
+
+      },
+      // deprecation warning
+      operatorsAliases: false,
+      //disable logging
+      logging: false
+
+    })
+    return connection
+  }
+}
+module.exports = Postgres
